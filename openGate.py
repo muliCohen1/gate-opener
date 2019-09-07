0f3e9d72f0145614fd3409649b1dd5a30b85aab1
mulic3 <mulic3@gmail.com>
2019-09-07 12:23:25 +0300

    specify documentation

--- openGate.py
+++ openGate.py
@@ -1,9 +1,9 @@
-from bottle import route, run, template, #request - not needed at stripped down version;
+from bottle import route, run, template, #request - not needed at stripped down version. muliCohen
 import RPi.GPIO as GPIO
 import time
 
 GPIO.setmode(GPIO.BCM)
-CONTROL_PIN = 18
+CONTROL_PIN = 18 #change on demand. muliCohen
 
 @route('/') 
 def index():
@@ -12,13 +12,13 @@ def index():
 @route('/on')
 def index():
     GPIO.setup(CONTROL_PIN, GPIO.OUT)
-    GPIO.output(CONTROL_PIN, False) #for supporting cheap chinese' relay;
+    GPIO.output(CONTROL_PIN, False) #for supporting cheap chinese' relay. muliCohen
     time.sleep(2)    
-    GPIO.setup(CONTROL_PIN, GPIO.IN) #for supporting reversed HIGH/LOW 
+    GPIO.setup(CONTROL_PIN, GPIO.IN) #for supporting reversed HIGH/LOW. muliCohen
     return template('home.html')
 
 try: 
     run(host='0.0.0.0', port=80)
 finally:  
     print('Cleaning up GPIO')
-    GPIO.cleanup() #resets to input mode - important!;
+    GPIO.cleanup() #resets to input mode - important! muliCohen
